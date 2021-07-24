6b05070d25c29ac3c86b26666d00c758ba09c4ba
Sanjay Idpuganti <sanjayidpuganti@outlook.com>
2021-07-24 20:59:26 +0530

    Added PreCommit Hook and Prettier

--- src/transactions/delete.ts
+++ src/transactions/delete.ts
@@ -1,29 +1,35 @@
-import { ArgPredicates, validators } from "./typeguards"
-import { DeleteArg, DeleteFn, DeleteHttpResponse, FirstArgument, RequestHandler, UpdateFn, UpdateHttpResponse } from "./types"
+import { ArgPredicates, validators } from './typeguards'
+import {
+  DeleteArg,
+  DeleteFn,
+  DeleteHttpResponse,
+  FirstArgument,
+  RequestHandler,
+  UpdateFn,
+  UpdateHttpResponse,
+} from './types'
 import ow from 'ow'
 
-import { HttpStatusCodes } from "../common/StatusCodes"
-import withStatus from "../internal/withStatus"
-import { store } from "./store"
-import { Result } from "@badrap/result"
+import { HttpStatusCodes } from '../common/StatusCodes'
+import withStatus from '../internal/withStatus'
+import { store } from './store'
+import { Result } from '@badrap/result'
 
-
-function assertDeleteRequest(req: unknown): asserts req is DeleteArg { 
+function assertDeleteRequest(req: unknown): asserts req is DeleteArg {
   const shapeObj: ArgPredicates<DeleteArg> = {
-    '_id': validators['_id'],
-    'username': validators['username']
+    _id: validators['_id'],
+    username: validators['username'],
   }
   const shape = ow.object.exactShape(shapeObj)
   ow(req, shape)
 }
 
 async function deleteTransaction(req: FirstArgument<DeleteFn>): Promise<Result<DeleteHttpResponse, Error>> {
-  const response = await store.delete(req);
+  const response = await store.delete(req)
   return withStatus(response, HttpStatusCodes.Success)
 }
 
-
 export const requestHandler: RequestHandler<FirstArgument<UpdateFn>, UpdateHttpResponse> = {
-	handler: deleteTransaction,
-	validator: assertDeleteRequest
+  handler: deleteTransaction,
+  validator: assertDeleteRequest,
 }
